b83019e11cc2b2fce5a8d01d2d15029a2fb945de
Pasqualecannavacciuolo <p.cannavacciuolo@icloud.com>
2024-06-19 16:19:01 +0200

    Cambio configurazione

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  safelist: ["text-[#FF6922]", "nav-link", "active-nav-link"],
   theme: {
     extend: {},
   },
